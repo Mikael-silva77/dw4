087a9d5ddb7f0c990088340ffdfe9af0f0d35d4d
mikael-silva77 <mkaelsoares9@gmail.com>
2025-06-06 08:30:38 -0400

    g

---
+++ src/routes/03/dummy/users/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    let { data } = $props();
+</script>
+
+<div class="container my-5">
+    <h1 class="mb-4">Usuários:</h1>
+
+    <ul class="list-group">
+        {#each data.users.users as user}
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <a href="/03/dummy/users/{user.id}" class="text-decoration-none">
+                    {user.firstName}
+                </a>
+                <span class="badge bg-primary rounded-pill">{user.address.country}</span>
+            </li>
+        {/each}
+    </ul>
+
+    <div class="text-center mt-4">
+        <button class="btn btn-secondary" on:click={() => history.back()}>
+            Voltar
+        </button>
+    </div>
+</div>
